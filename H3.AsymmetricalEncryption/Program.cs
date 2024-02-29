50e9595c0a421f840b5b8d4c74beb2a483e8abdf
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2024-02-29 11:56:08 +0100

    Add project files.

---
+++ H3.AsymmetricalEncryption/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace H3.AsymmetricalEncryption
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter exponent: ");
+			string exponent = Console.ReadLine();
+
+			Console.WriteLine("Enter modulus: ");
+			string modulus = Console.ReadLine();
+
+			Console.WriteLine("Enter message: ");
+			string message = Console.ReadLine();
+
+			// Encrypt the message using the supplied exponent and modulus
+			byte[] encryptedBytes = Encrypt(Encoding.UTF8.GetBytes(message), exponent, modulus);
+
+			// Convert the encrypted bytes to hex
+			string encryptedMessage = BitConverter.ToString(encryptedBytes);
+
+			Console.WriteLine("Encrypted message: " + encryptedMessage);
+
+			Console.ReadKey();
+		}
+
+		static byte[] Encrypt(byte[] messageBytes, string exponent, string modulus)
+		{
+			using RSA rsa = RSA.Create();
+			RSAParameters rsaParams = new RSAParameters
+			{
+				Exponent = ParseHexBytes(exponent),
+				Modulus = ParseHexBytes(modulus)
+			};
+			rsa.ImportParameters(rsaParams);
+
+			return rsa.Encrypt(messageBytes, RSAEncryptionPadding.OaepSHA256);
+		}
+
+		static byte[] ParseHexBytes(string hex)
+		{
+			hex = hex.Replace("-", "");
+			byte[] bytes = new byte[hex.Length / 2];
+			for (int i = 0; i < hex.Length; i += 2)
+			{
+				bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+			}
+			return bytes;
+		}
+	}
+}
